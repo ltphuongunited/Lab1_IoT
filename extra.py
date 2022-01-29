a95a70a5f010f07fb4e82b6b55bdfa1deae2e5fc
ltphuongunited <68008652+ltphuongunited@users.noreply.github.com>
2022-01-29 22:33:51 +0700

    third

--- extra.py
+++ extra.py
@@ -2,11 +2,11 @@ print("Xin chào ThingsBoard")
 import paho.mqtt.client as mqttclient
 import time
 import json
-
+import subprocess as sp
 
 BROKER_ADDRESS = "demo.thingsboard.io"
 PORT = 1883
-THINGS_BOARD_ACCESS_TOKEN = "FKvP9eG2VhLpA7RiMcbV"
+THINGS_BOARD_ACCESS_TOKEN = "1shuYg6dZunCks9ediO5"
 
 
 def subscribed(client, userdata, mid, granted_qos):
@@ -43,17 +43,31 @@ client.loop_start()
 client.on_subscribe = subscribed
 client.on_message = recv_message
 
-temp = 30
-humi = 50
-longitude = 106.6297
-latitude = 10.8231
-
-
-counter = 0
 
 while True:
-    collect_data = {'temperature': temp, 'humidity': humi,'longitude': longitude, 'latitude': latitude}
-    temp += 1
-    humi += 1
+    accuracy = 3
+    pshellcomm = ['powershell']
+    pshellcomm.append('add-type -assemblyname system.device; ' \
+                      '$loc = new-object system.device.location.geocoordinatewatcher;' \
+                      '$loc.start(); ' \
+                      'while(($loc.status -ne "Ready") -and ($loc.permission -ne "Denied")) ' \
+                      '{start-sleep -milliseconds 100}; ' \
+                      '$acc = %d; ' \
+                      'while($loc.position.location.horizontalaccuracy -gt $acc) ' \
+                      '{start-sleep -milliseconds 100; $acc = [math]::Round($acc*1.5)}; ' \
+                      '$loc.position.location.latitude; ' \
+                      '$loc.position.location.longitude; ' \
+                      '$loc.position.location.horizontalaccuracy; ' \
+                      '$loc.stop()' % (accuracy))
+
+    p = sp.Popen(pshellcomm, stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.STDOUT, text=True)
+    (out, err) = p.communicate()
+    out = out.split('\n')
+
+    lat = float(out[0])
+    long = float(out[1])
+
+    collect_data = {'longitude': long, 'latitude': lat}
+
     client.publish('v1/devices/me/telemetry', json.dumps(collect_data), 1)
     time.sleep(10)
